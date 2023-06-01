68f5ffc9d953bf157f121454d0075413bbfb57d7
SiaESin <siabhon.sindab@gmail.com>
2023-05-31 20:48:40 -0700

    add views and layouts

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
+// BREADS
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+//MIDDLEWARE
+application.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //ROUTES
 app.get('/', (req, res)=>{
     res.send('Welcome to the Bread App!')
 })
-
-// BREADS
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
 
 app.listen(PORT, ()=>{
     console.log('listening on port:', PORT)
