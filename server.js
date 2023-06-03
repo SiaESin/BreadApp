bde4436bf9e2ddb0d8b3a656e0773ad4f816d065
SiaESin <siabhon.sindab@gmail.com>
2023-06-03 10:59:35 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,19 +6,24 @@ require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
-// BREADS
+//MIDDLEWARE
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
-
-//MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 //ROUTES
 app.get('/', (req, res)=>{
     res.send('Welcome to the Bread App!')
 })
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 
 app.listen(PORT, ()=>{
     console.log('listening on port:', PORT)
