c9d10c2287b06c06c2c06835bc101e2166a92adb
SiaESin <siabhon.sindab@gmail.com>
2023-06-05 19:11:15 -0700

    add delete option

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -23,7 +23,8 @@ breads.get('/:arrayIndex', (req, res)=>{
     // res.send(Bread[req.params.arrayIndex])
     if(Bread[req.params.arrayIndex]){
     res.render('Show',{
-        bread: Bread[req.params.arrayIndex]
+        bread: Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex,
     })
     }else{
         res.send('404')
@@ -45,5 +46,11 @@ breads.post('/', express.urlencoded({extended: true}), (req,res) => {
     res.redirect('/breads')
   })
   
+  // DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
   
 module.exports = breads
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 //configuration 
 require('dotenv').config()
@@ -8,9 +9,11 @@ console.log(PORT)
 
 //MIDDLEWARE
 const breadsController = require('./controllers/breads_controller.js')
+app.use(methodOverride('_method'))
 app.use('/breads', breadsController)
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
