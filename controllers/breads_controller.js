f266a8dd0262aaeb3d6da7347b162f2f59119366
SiaESin <siabhon.sindab@gmail.com>
2023-06-10 13:24:23 -0700

    adding mongoDB

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,15 @@ const Bread = require('../models/bread.js')
 
 //INDEX
 breads.get('/', (req, res)=>{
-    res.render('Index',
-    {
-        breads: Bread,
-        title: 'Index Page'
-    }
-    )
+    Bread.find()
+    .then(foundBreads => {
+      res.render('index',
+        {
+            breads: foundBreads,
+            title: 'Index Page'
+        })
+      
+    })
     //res.send(Bread)
 })
 breads.get('/:indexArray/edit', (req, res) =>{
@@ -24,22 +27,31 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res)=>{
-    // res.send(Bread[req.params.arrayIndex])
-    if(Bread[req.params.arrayIndex]){
-    res.render('Show',{
-        bread: Bread[req.params.arrayIndex],
-        index: req.params.arrayIndex,
-    })
-    }else{
-        res.send('404')
-    }
+breads.get('/:id', (req, res)=>{
+    Bread.findById(req.params.id)
+      .then(foundBread => {
+        res.render('Show', {
+          bread: foundBread
+        })
+      })
+      .catch(err => {
+        res.send('<h1>404: Please hang up and try again...</h1>')
+      })
+  // res.send(Bread[req.params.arrayIndex])
+    // if(Bread[req.params.arrayIndex]){
+    // res.render('Show',{
+    //     bread: Bread[req.params.arrayIndex],
+    //     index: req.params.arrayIndex,
+    // })
+    // }else{
+    //     res.send('404')
+    // }
 })
 // CREATE
 breads.post('/', express.urlencoded({extended: true}), (req,res) => {
-    //console.log(req.body)
+    console.log('undefined')
     if (!req.body.image) {
-        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+        req.body.image = undefined
       }
     
     if(req.body.hasGluten === 'on') {
@@ -47,7 +59,7 @@ breads.post('/', express.urlencoded({extended: true}), (req,res) => {
     } else {
       req.body.hasGluten = 'false'
     }
-    Bread.push(req.body)
+    Bread.create(req.body)
     res.redirect('/breads')
   })
 // UPDATE
